c3dcd6b845f3c0bbc5766f846b55b4ca1277dd9c
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-07-26 13:06:14 +0300

    Form

---
+++ js/donate.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var donateForm = document.getElementById('Donateform');
+donateForm.addEventListener('submit', createBook);
+console.log("before", allBooks);
+function createBook() {
+    event.preventDefault();
+    var title = event.target.title.value;
+    var author = event.target.author.value;
+    var copies = Number(event.target.copies.value);
+    var city = event.target.city.value;
+    var location = event.target.dropLocation.value;
+    var number = event.target.phoneNumber.value;
+
+    new Book (title,author,copies,city,location,number)
+
+
+    var jsonStringBooks = JSON.stringify(allBooks);
+
+    localStorage.setItem('allBooks',jsonStringBooks);
+};
+
+
+// var perviousArray = ;
