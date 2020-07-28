573f21946c4203a8f30d3434508e7687fbbf3a53
dinaAlsaid <dinamalsaid@gmail.com>
2020-07-28 13:38:05 +0300

    fliter the books by category

--- js/library.js
+++ js/library.js
@@ -1,57 +1,44 @@
 'use strict'
 
-console.log(allBooks);
 var columnOne = document.getElementById('column1');
 var columnTwo = document.getElementById('column2');
-var columnThree = document.getElementById('column3');
+var catDiv = document.getElementById("catButtons");
+var filteredBooks = [];
+console.log(allBooks);
 
+// create a div that has the books info 
+function generateBook(index,array){
+    var bookParent = document.createElement('div');
 
-for (let index = 0; index < allBooks.length; index++) {
-    if (index % 3 == 0) {
-        var bookParent = document.createElement('div');
-        generateBook(index);
-        columnOne.appendChild(bookParent);
-    }
-    else if (index % 3 == 1) {
-        var bookParent = document.createElement('div');
-        generateBook(index);
-        columnTwo.appendChild(bookParent);
-
-    }
-    else {
-        var bookParent = document.createElement('div');
-        generateBook(index);
-        columnThree.appendChild(bookParent);
-    }
-};
-
-
-function generateBook(index) {
-    bookParent.setAttribute('class', 'book')
     var image = document.createElement('img');
-    image.setAttribute('class', 'cover')
-    image.setAttribute('src', 'https://place-hold.it/200x250')
     var info = document.createElement('ul');
-    info.setAttribute('class', 'infoList')
     var title = document.createElement('li');
-    title.innerHTML = allBooks[index].title;
     var author = document.createElement('li');
-    author.innerHTML = allBooks[index].author;
     var categories = document.createElement('li');
-    categories.innerHTML = allBooks[index].categories;
-
-    var remove = document.createElement('button')
-    remove.textContent = 'take book'
-    remove.setAttribute('onclick', 'removeBook(' + index + ')')
-
     var copies = document.createElement('li');
-    copies.innerHTML = allBooks[index].copies;
     var city = document.createElement('li');
-    city.innerHTML = allBooks[index].city;
     var location = document.createElement('li');
-    location.innerHTML = allBooks[index].location;
     var number = document.createElement('li');
-    number.innerHTML = allBooks[index].number;
+
+    //in the div
+    bookParent.setAttribute('class', 'book');
+    image.setAttribute('class', 'cover');
+    image.setAttribute('src', 'https://place-hold.it/200x250');
+    info.setAttribute('class', 'infoList');
+
+    //li elements
+    title.innerHTML = array[index].title;
+    author.innerHTML = array[index].author;
+    categories.innerHTML = array[index].categories;
+    copies.innerHTML = array[index].copies;
+    city.innerHTML = array[index].city;
+    location.innerHTML = array[index].location;
+    number.innerHTML = array[index].number;
+    
+    //button w/ added an onClick event
+    var remove = document.createElement('button');
+    remove.textContent = 'take book';
+    remove.setAttribute('onclick', 'removeBook(' + index + ')');
 
     info.append(title);
     info.append(author);
@@ -64,7 +51,26 @@ function generateBook(index) {
     bookParent.append(info);
     bookParent.append(remove);
 
+    return bookParent;
 }
+
+
+// append the created div that has the info to the two columns in the html page
+function fillCol( array ){
+    for (let index = 0; index < array.length; index++) {
+        var childdiv;
+        if (index % 2 == 0) {
+            childdiv=generateBook(index,array);
+            columnOne.appendChild(childdiv);
+        }else{
+            childdiv=generateBook(index,array);
+            columnTwo.appendChild(childdiv);
+        }
+    }
+}
+
+
+// remove book from the list of availabe books (allBooks) permenantly 
 function removeBook(index) {
     console.log('Start of function');
     var allBooks = JSON.parse(localStorage.getItem('allBooks'));
@@ -73,3 +79,38 @@ function removeBook(index) {
     localStorage.setItem("allBooks", allBooks);
     location.reload();
 }
+
+// fill the filtered books depending on categories
+function filterByCat( categ ){
+    filteredBooks = [];
+    for (let i = 0; i < allBooks.length; i++) {
+        if (allBooks[i].categories == categ ) {
+            filteredBooks.push(allBooks[i]);
+        }
+    }
+}
+
+// clear the content of the columns
+function clearCol(){
+    columnOne.textContent="";
+    columnTwo.textContent="";
+}
+
+
+catDiv.addEventListener('click',generateBookByCat);
+function generateBookByCat(){
+
+    clearCol();
+    var catId = event.target.id;
+    if (catId !== "allCat") {
+        filterByCat(catId);
+        fillCol(filteredBooks);    
+    } else {
+        fillCol(allBooks);
+    }
+
+}
+
+// main function here 
+
+fillCol(allBooks);
