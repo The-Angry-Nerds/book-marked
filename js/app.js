6b78c03c9c0d713b6ad0c5d49ebf7180d79ab427
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-08-04 14:10:12 +0300

    fixed url

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 'use strict'
 
-if (localStorage.getItem("name") === null ){
-    window.location.replace('../pages/user.html');
+console.log(window.location.href);
+
+
+
+if (localStorage.getItem("name") === null) {
+    if (window.location.href == "https://the-angry-nerds.github.io/book-marked/index.html") {
+        window.location.replace('/pages/user.html');
+    }
+    else {
+        window.location.replace('../pages/user.html');
+    }
 }
 
 var allBooks = [];
@@ -9,7 +18,7 @@ var filteredBooks = [];
 // var allTitles = [];
 var user = document.getElementById("user");
 
-user.textContent=`Welcome ${localStorage.getItem("name")}`;
+user.textContent = `Welcome ${localStorage.getItem("name")}`;
 
 if (localStorage.getItem('allBooks') !== null) {
     allBooks = JSON.parse(localStorage.getItem('allBooks'));
@@ -19,23 +28,23 @@ if (localStorage.getItem('allTitles') !== null) {
 };
 
 // Book object constructor
-function Book(title,author,copies,city,number,categories){
+function Book(title, author, copies, city, number, categories) {
     this.title = title;
     this.author = author;
     this.copies = copies;
     this.city = city;
     this.number = number;
-    this.categories=categories;
+    this.categories = categories;
     this.taken = false;
     // allTitles.push(this.title) ; 
     allBooks.push(this);
 }
 
 // fill the filtered books depending on categories
-function filterByCat( categ ){
+function filterByCat(categ) {
     filteredBooks = [];
     for (let i = 0; i < allBooks.length; i++) {
-        if (allBooks[i].categories == categ ) {
+        if (allBooks[i].categories == categ) {
             filteredBooks.push(allBooks[i]);
         }
     }
